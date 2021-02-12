077bd9a5fb7f897f636acf201c08db3850461fba
Muthukumar <p.muthubalu@gmail.com>
2021-02-12 19:16:37 +0530

    Smallest number from a list

--- sum1.py
+++ sum1.py
@@ -1,20 +1,30 @@
 #!/bash/python
-print('PRG=1')
+print('#######')
+print('PRG-->1 ++Sum of all the items in a list++')
 num=[1,2,3]
 tot=0
 for i in num:
     tot += i
 print(tot)
-print('#######\n')
 
-print('PRG=2')
+print('\n#######')
+print('PRG-->2 ++Sum of all the items in a list++')
 num=[1,2,3]
 tot=0
 print (len(num))
 for i in range(len(num)):
     tot +=num[i]
 print(tot)
-print('#######\n')
 
-print('PRG=3')
+print('\n#######')
+print('PRG-->3 ++Smallest number from list++')
+num=[1,0,2,3]
+out=num[0]
+print (len(num))
+for i in range(1,len(num)):
+    if out<=num[i]:
+        continue
+    else:
+        out=num[i]
+print(out)
 
