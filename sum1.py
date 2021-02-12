2a904f6789fc739124b1dbcb5c976cf33f21df9f
Muthukumar <p.muthubalu@gmail.com>
2021-02-12 16:45:33 +0530

    sum of the elements in list

---
+++ sum1.py
@@ -0,0 +1,20 @@
+#!/bash/python
+print('PRG=1')
+num=[1,2,3]
+tot=0
+for i in num:
+    tot += i
+print(tot)
+print('#######\n')
+
+print('PRG=2')
+num=[1,2,3]
+tot=0
+print (len(num))
+for i in range(len(num)):
+    tot +=num[i]
+print(tot)
+print('#######\n')
+
+print('PRG=3')
+
